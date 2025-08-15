0905cb9c8636f3635c9f9a6601db14da74a842f5
Chris Du <hailong.du8889@gmail.com>
2025-08-15 18:08:19 +0800

    update libs

--- api/index.py
+++ api/index.py
@@ -1,3 +1,5 @@
 # api/index.py
-from main import app as _app  # main.py 里已有 FastAPI() 的实例 app
-app = _app                    # Vercel Python Runtime 会按 ASGI 调用这个 app
+from main import app as fastapi_app
+
+# Vercel Python Runtime 要求暴露一个名为 `app` 的 ASGI 对象
+app = fastapi_app
