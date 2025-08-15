9beed976ade3d53d51f3aa6e729013b037863b8a
Chris Du <hailong.du8889@gmail.com>
2025-08-15 16:13:41 +0800

    reset

--- api/index.py
+++ api/index.py
@@ -1,5 +1,3 @@
 # api/index.py
-from asgi_vercel import AsgiHandler
-from main import app as fastapi_app
-
-handler = AsgiHandler(fastapi_app)
+from main import app as _app  # main.py 里已有 FastAPI() 的实例 app
+app = _app                    # Vercel Python Runtime 会按 ASGI 调用这个 app
